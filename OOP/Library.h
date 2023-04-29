342f434291d6a933aadc59199fc924be2386f12c
Даша Побережник <dasosha2017@gmail.com>
2023-04-29 23:52:55 +0300

    +

--- OOP/Library.h
+++ OOP/Library.h
@@ -1,1 +1,4 @@
-#pragma once
+#include<iostream>
+#include<Windows.h>
+#include<conio.h>
+using namespace std;
---
+++ OOP/Main.cpp
@@ -0,0 +1,91 @@
+#include"menu.h"
+#include "Class.h"
+
+int main() {
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 7));
+
+	Menu* menu = new Menu;
+	menu->Vmenu();
+	char switch_on;
+
+	do
+	{
+		switch_on = _getch();
+		switch (switch_on)
+		{
+		case '0':
+		{
+			break;
+		}break;
+		case '1':
+		{
+			CharStack stack(5);
+			stack.push('a');
+			stack.push('b');
+			stack.push('c');
+			stack.push('d');
+			stack.push('e');
+
+			cout << "Count: " << stack.count() << endl;
+			cout << "Is empty? " << stack.isEmpty() << endl;
+			cout << "Is full? " << stack.isFull() << endl;
+
+			while (!stack.isEmpty()) {
+				cout << stack.pop() << " ";
+			}
+			cout << endl;
+
+			stack.push('f');
+			stack.push('g');
+			cout << "Peek: " << stack.peek() << endl;
+			stack.pop();
+			cout << "Peek: " << stack.peek() << endl;
+
+			stack.clear();
+			cout << "Count: " << stack.count() << endl;
+			cout << "Is empty? " << stack.isEmpty() << endl;
+			cout << "Is full? " << stack.isFull() << endl;
+
+		}break;
+		case '2':
+		{
+			DynamicStack stack(5);
+
+			stack.push('a');
+			stack.push('b');
+			stack.push('c');
+			stack.push('d');
+			stack.push('e');
+			stack.push('f');
+			stack.push('g');
+
+			cout << "Count: " << stack.count() << endl;
+			cout << "Is empty? " << stack.isEmpty() << endl;
+			cout << "Is full? " << stack.isFull() << endl;
+
+			while (!stack.isEmpty()) {
+				cout << stack.pop() << " ";
+			}
+			cout << endl;
+
+			cout << "Peek: " << stack.peek() << endl;
+			stack.pop();
+			cout << "Peek: " << stack.peek() << endl;
+
+			stack.clear();
+			cout << "Count: " << stack.count() << endl;
+			cout << "Is empty? " << stack.isEmpty() << endl;
+			cout << "Is full? " << stack.isFull() << endl;
+
+		}break;
+		}
+
+
+	} while (switch_on != '0');
+	return 0;
+}
