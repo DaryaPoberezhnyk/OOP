08f4f7c5b719fffba11b8fd31a7f0815999f26d9
Даша Побережник <dasosha2017@gmail.com>
2023-04-29 23:27:32 +0300

    +

--- OOP/Menu.h
+++ OOP/Menu.h
@@ -1,1 +1,42 @@
-#pragma once
+#include"Library.h"
+
+class Menu {
+public:
+	void Vmenu() {
+		cout << " #===============================# " << endl;
+		cout << "  |                             |  " << endl;
+		cout << "  |      1 - class Точка        |  " << endl;
+		cout << "  |                             |  " << endl;
+		cout << "  |      2 - class Дріб         |  " << endl;
+		cout << "  |                             |  " << endl;
+		cout << "  |      3 - class Студент      |  " << endl;
+		cout << "  |                             |  " << endl;
+		cout << "  |     4 - Телефонна книга     |  " << endl;
+		cout << "  |                             |  " << endl;
+		cout << " #===============================# " << endl;
+	}
+	void MenuPhone() {
+		cout << endl;
+		cout << "                                                  " << endl;
+		cout << "     Для додання нового абонента натисніть -- 1   " << endl;
+		cout << "                                                  " << endl;
+		cout << "     Для Видалення абонента натисніть -- 2        " << endl;
+		cout << "                                                  " << endl;
+		cout << "     Для пошуку абонента натисніть -- 3           " << endl;
+		cout << "                                                  " << endl << endl;
+	}
+	void DeleteMenu() {
+		cout << "               " << endl;
+		cout << "    1 -- Bob   " << endl;
+		cout << "    2 -- Daria " << endl;
+		cout << "    3 -- Tom   " << endl;
+		cout << "               " << endl;
+	}
+	void DeleteMenu1() {
+		cout << "    1 --       " << endl;
+		cout << "    2 -- Bob   " << endl;
+		cout << "    3 -- Daria " << endl;
+		cout << "    4 -- Tom   " << endl;
+		cout << "               " << endl;
+	}
+};
